2760159886567ce3ed308f8e9e5d608b193c3dcb
Robert Reiss <reissaudiodesign@yahoo.com>
2019-10-11 16:29:10 -0600

    finished project

---
+++ src/components/Topics/Palindrome.js
@@ -0,0 +1,41 @@
+import React, {Component} from 'react'
+
+export default class Palindrome extends Component{
+    constructor(){
+        super();
+        this.state = {
+            userInput: "",
+            palindrome: ""
+        }
+    }
+
+    changeInput(input){
+        this.setState({userInput: input})
+    }
+
+    solve(input){
+        let forwards = input
+        let backwards = [...input]
+        let palindrome = ''
+        backwards = backwards.reverse();
+        backwards = backwards.join('');
+        if(forwards === backwards){
+            palindrome = 'yes'
+        } else {
+            palindrome = 'no'
+        }
+        this.setState({palindrome: palindrome})
+    }
+
+    render(){
+        return(
+            <div className="puzzleBox filterStringPB">
+                <h4>Palindrome</h4>
+                <input className="inputLine" onChange={(event) => this.changeInput(event.target.value)}/>
+                <button className="confirmationButton" onClick={() => this.solve(this.state.userInput)}>Is it a palindrome?</button>
+                <span className="resultsBox">{this.state.palindrome}</span>
+            </div>
+            )
+        }
+    }
+    
